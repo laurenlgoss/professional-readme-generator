eb924a906af0a4ffa4f4080b8a5b91943d0b25fa
Lauren Goss <laurenlgoss98@gmail.com>
2021-08-06 15:16:50 -0400

    added first inquirer prompt

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Enter project title:",
+        name: "title",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) =>
+            console.log(response.title)
+        );
+}
 
 // Function call to initialize app
 init();
